efeca975cb6d9d8db0973d9f5f1046ef806e8db6
Lea Hergert <lhergert@cursorinsight.com>
2022-08-04 12:36:55 +0200

    docs: Add LICENSE.md and update headers

--- src/Dumper.jl
+++ src/Dumper.jl
@@ -1,7 +1,7 @@
 ###-----------------------------------------------------------------------------
-### Copyright (C) 2022- Cursor Insight
+### Copyright (C) Dumper.jl
 ###
-### All rights reserved.
+### SPDX-License-Identifier: MIT License
 ###-----------------------------------------------------------------------------
 
 module Dumper
